44ebfcf769ceb151df3a19c7dcc277cca85e1a59
zmoon <zmoon92@gmail.com>
2023-09-07 11:52:22 -0600

    Bit more robust

--- attrs.py
+++ attrs.py
@@ -13,11 +13,20 @@ for d in attrs["daily"]["columns"]:
 
 def expand_str(s: str) -> list[str]:
     import re
+    from ast import literal_eval
 
     repls: dict[str, list[str]] = {}
     to_repl = re.findall(r"\{.*?\}", s)
     for braced in to_repl:
-        opts = braced[1:-1].split(",")
+        opts = [s.strip() for s in braced[1:-1].split(",")]
+        for i, opt in enumerate(opts):
+            # Maybe remove quotes
+            try:
+                opt_ = literal_eval(opt)
+            except ValueError:
+                continue
+            else:
+                opts[i] = opt_
         repls[braced] = opts
 
     if not repls:
@@ -40,8 +49,23 @@ def expand_str(s: str) -> list[str]:
     return s_news
 
 
+s = "hi no opts"
+assert expand_str(s) == ["hi no opts"]
+
 s = "hi {only-one-opt}"
 assert expand_str(s) == ["hi only-one-opt"]
+
+s = "{one,two}"
+assert expand_str(s) == ["one", "two"]
+
+s = "{one,'two'}"
+assert expand_str(s) == ["one", "two"]
+
+s = "{one, 'two'}"
+assert expand_str(s) == ["one", "two"]
+
+s = "{one, ' two'}"
+assert expand_str(s) == ["one", " two"]
 
 s = "hi there, I'm a {cat,dog}. {woof,meow}!"
 print(s, "=>", expand_str(s))
