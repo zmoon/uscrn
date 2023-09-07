d00b84ff60cc275162d5380a79a8df4d6d8014e7
zmoon <zmoon92@gmail.com>
2023-09-07 14:25:51 -0600

    Initial flit config

--- uscrn/__init__.py
+++ uscrn/__init__.py
@@ -2,10 +2,12 @@
 Load US CRN data.
 """
 
+__version__ = "0.1.0.dev0"
+
 from .attrs import load_attrs
 from .get import get_crn, load_meta, read_daily, to_xarray
 
 ATTRS = load_attrs()
 del load_attrs
 
-__all__ = ["load_meta", "read_daily", "get_crn", "to_xarray", "ATTRS"]
+__all__ = ["load_meta", "read_daily", "get_crn", "to_xarray", "ATTRS", "__version__"]
