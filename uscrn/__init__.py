bddeb0f2c6c2a592c4e48c72ab58806a5c35ae7b
zmoon <zmoon92@gmail.com>
2023-09-21 13:08:28 -0600

    subhourly reader
    seems to be working

--- uscrn/__init__.py
+++ uscrn/__init__.py
@@ -5,7 +5,16 @@ Load U.S. CRN data.
 __version__ = "0.1.0.dev4"
 
 from .attrs import load_attrs
-from .data import get_data, load_meta, read, read_daily, read_hourly, read_monthly, to_xarray
+from .data import (
+    get_data,
+    load_meta,
+    read,
+    read_daily,
+    read_hourly,
+    read_monthly,
+    read_subhourly,
+    to_xarray,
+)
 
 ATTRS = load_attrs()
 """Dataset and variable attributes, mainly taken from the respective readmes.
@@ -22,6 +31,7 @@ __all__ = [
     "read_daily",
     "read_hourly",
     "read_monthly",
+    "read_subhourly",
     "to_xarray",
     "__version__",
 ]
