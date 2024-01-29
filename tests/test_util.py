2158e60272a716ba5f9d29614d4aed5bd1ccc26a
zmoon <zmoon92@gmail.com>
2024-01-29 10:42:21 -0700

    Add result

--- tests/test_util.py
+++ tests/test_util.py
@@ -12,9 +12,12 @@ def test_retry(caplog):
         if n < 2:
             n += 1
             raise ConnectionError
+        return "result"
 
     with caplog.at_level("INFO"):
-        func()
+        res = func()
 
     for r in caplog.records:
         assert r.getMessage() == "Retrying func in 1 s after connection error"
+
+    assert res == "result"
